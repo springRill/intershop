0a40f2e4ce46b9af1741e209cdd6c02c01cc3bf3
springRill <springRill@gmail.com>
2025-07-17 15:15:47 +0300

    remove buttons for unauth users

--- showcase/src/main/java/com/intershop/configuration/SecurityConfiguration.java
+++ showcase/src/main/java/com/intershop/configuration/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
@@ -20,6 +21,8 @@ import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClient
 import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebSession;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
@@ -59,13 +62,32 @@ public class SecurityConfiguration {
 //                .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .logout(logout -> logout
-                        .logoutSuccessHandler((webFilterExchange, authentication) ->
+                                .logoutSuccessHandler((webFilterExchange, authentication) ->
+                                        {
+                                            ServerWebExchange exchange = webFilterExchange.getExchange();
+
+                                            return exchange.getSession()
+                                                    .flatMap(WebSession::invalidate)
+                                                    .then(Mono.fromRunnable(() -> {
+                                                        ResponseCookie deleteCookie = ResponseCookie.from("JSESSIONID", "")
+                                                                .path("/")
+                                                                .maxAge(0)
+                                                                .build();
+                                                        exchange.getResponse().addCookie(deleteCookie);
+
+                                                        exchange.getResponse().setStatusCode(HttpStatus.SEE_OTHER);
+                                                        exchange.getResponse().getHeaders().setLocation(URI.create("/"));
+                                                    }));
+
+                                        }
+/*
                                 Mono.fromRunnable(() -> {
                                     ServerHttpResponse response = webFilterExchange.getExchange().getResponse();
                                     response.setStatusCode(HttpStatus.SEE_OTHER);
                                     response.getHeaders().setLocation(URI.create("/"));
                                 })
-                        )
+*/
+                                )
                 )
                 .build();
 
